2c9f3f93b65e58187b10b75c146aadddbb21eb10
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2023-09-11 16:44:03 +0530

    Update auto-sign.sh

--- auto-sign.sh
+++ auto-sign.sh
@@ -1,5 +1,4 @@
 sudo dnf install kmodtool akmods mokutil openssl -y
 sudo kmodgenca -a
 sudo mokutil --import /etc/pki/akmods/certs/public_key.der
-sudo reboot
-
+printf "Reboot now and enroll with the password!!"
